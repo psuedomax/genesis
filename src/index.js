52cb53f1f4b059e7a61a4b9afb6c3c7094f6f782
psuedomax <40203433+psuedomax@users.noreply.github.com>
2019-03-27 18:07:12 +0700

    Add Scene from Orbit work

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@
 const SDK = window.requirejs('w3reality-sdk');
 // console.log('SDK:', SDK);
 
+require('../css/style.css');
+
+var _sceneController = require('../js/controllers/scene-controller');
+
+var _sceneController2 = _interopRequireDefault(_sceneController);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var scene = new _sceneController2.default();
+
+
 class App extends SDK.App {
     // override
     static createWorld() {
@@ -16,6 +27,7 @@ class App extends SDK.App {
     constructor(data, name="foo") {
         super(data, name);
         this.set_block(0, 0, 4, BLOCK.MODEL_TEST);
+        this.setScene(scene);
         this.say('welcome');
     }
 
